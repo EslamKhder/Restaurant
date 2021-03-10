e4e91d47aac884bcdf86090fc2d52118a1093dd2
EslamKhder <eslamkhder81@gmail.com>
2021-03-10 08:37:19 +0200

    create back-end Project

---
+++ back-end/src/main/java/com/spring/restaurant/RestaurantProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantProjectApplication.class, args);
+    }
+
+}
