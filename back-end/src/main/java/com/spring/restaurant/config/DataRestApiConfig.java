849e7b08507a060193d52c586c94dfbc82c418be
EslamKhder <eslamkhder81@gmail.com>
2021-03-26 17:12:27 +0200

    I am Angry Closes #20

--- back-end/src/main/java/com/spring/restaurant/config/DataRestApiConfig.java
+++ back-end/src/main/java/com/spring/restaurant/config/DataRestApiConfig.java
@@ -13,7 +13,10 @@ public class DataRestApiConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         HttpMethod[] preventMethod = {HttpMethod.GET,HttpMethod.POST,HttpMethod.PUT,HttpMethod.DELETE};
-        config.getExposureConfiguration()
+
+        disableHttpMethod(Category.class,config,preventMethod);
+        disableHttpMethod(Order.class,config,preventMethod);
+        /*config.getExposureConfiguration()
               .forDomainType(Category.class)
               .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(preventMethod)))
               .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(preventMethod));
@@ -22,6 +25,12 @@ public class DataRestApiConfig implements RepositoryRestConfigurer {
                 .forDomainType(Order.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(preventMethod)))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(preventMethod));
-
+                */
+    }
+    private void disableHttpMethod(Class theClass, RepositoryRestConfiguration config,HttpMethod[] unsupportedMethod){
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedMethod)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethod));
     }
 }
