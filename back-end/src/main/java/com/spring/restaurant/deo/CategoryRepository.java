8ef986601536e9e90fe07aab7266aa5a074df6ce
EslamKhder <eslamkhder81@gmail.com>
2021-03-26 13:52:59 +0200

    Create Repository of (Category & Orders)  Closes #16

---
+++ back-end/src/main/java/com/spring/restaurant/deo/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.spring.restaurant.deo;
+
+import com.spring.restaurant.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
---
+++ back-end/src/main/java/com/spring/restaurant/deo/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.spring.restaurant.deo;
+
+import com.spring.restaurant.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}
