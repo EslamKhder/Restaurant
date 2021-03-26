95130daf729ee072902f50bb2d0264ce0e30f54e
EslamKhder <eslamkhder81@gmail.com>
2021-03-26 11:51:27 +0200

    add images and data to tables Closes #15

---
+++ files/sql/category.sql
@@ -0,0 +1,8 @@
+-- -----------------------------------------------------
+-- Categories
+-- -----------------------------------------------------
+INSERT INTO category(name,data_create) VALUES ('Foods',now());
+INSERT INTO category(name,data_create) VALUES ('Cold Drinks',now());
+INSERT INTO category(name,data_create) VALUES ('Hot Drinks',now());
+INSERT INTO category(name,data_create) VALUES ('Sweets',now());
+INSERT INTO category(name,data_create) VALUES ('Other Foods',now());
