660f096a65e25a92865275a15cd40929446e0ec3
EslamKhder <eslamkhder81@gmail.com>
2021-03-26 17:23:38 +0200

    Create Service of (Category & Orders) Closes #17

---
+++ back-end/src/main/java/com/spring/restaurant/service/CategoryService.java
@@ -0,0 +1,15 @@
+package com.spring.restaurant.service;
+
+import com.spring.restaurant.deo.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+}
---
+++ back-end/src/main/java/com/spring/restaurant/service/OrderService.java
@@ -0,0 +1,16 @@
+package com.spring.restaurant.service;
+
+import com.spring.restaurant.deo.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+}
