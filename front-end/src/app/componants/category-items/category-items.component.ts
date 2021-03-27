84f5a7f041b4b90475dd1c3e1d023d823b403d2e
EslamKhder <eslamkhder81@gmail.com>
2021-03-27 21:23:54 +0200

    reatrive category from database Closes #34

--- front-end/src/app/componants/category-items/category-items.component.ts
+++ front-end/src/app/componants/category-items/category-items.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CategoryServiceService} from '../../service/category-service.service';
+import {Category} from '../../model/category';
 
 @Component({
   selector: 'app-category-items',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryItemsComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+  constructor(private categoryService: CategoryServiceService) { }
 
   ngOnInit(): void {
+    this.getAllCategories();
+  }
+
+  getAllCategories(){
+    this.categoryService.getAllCategories().subscribe(
+      data => {
+        this.categories = data
+      }
+    )
   }
 
 }
