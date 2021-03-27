3a1d44c40cc0fc4374ebc96efd34f7239f92e422
EslamKhder <eslamkhder81@gmail.com>
2021-03-27 11:43:07 +0200

    show images of Orders Closes #29

--- front-end/src/app/model/order.ts
+++ front-end/src/app/model/order.ts
@@ -5,17 +5,17 @@ export class Order {
   data_create: Date;
   data_update: Date;
   description: string;
-  image: string;
+  img: string;
   price: number;
 
 
-  constructor(id: number, name: string, data_create: Date, data_update: Date, description: string, image: string, price: number) {
+  constructor(id: number, name: string, data_create: Date, data_update: Date, description: string, img: string, price: number) {
     this.id = id;
     this.name = name;
     this.data_create = data_create;
     this.data_update = data_update;
     this.description = description;
-    this.image = image;
+    this.img = img;
     this.price = price;
   }
 }
