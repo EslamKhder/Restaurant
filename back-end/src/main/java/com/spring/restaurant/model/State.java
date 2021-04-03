ba7c3155e3b1e0355a89b4da37d4016bc17d5f75
EslamKhder <eslamkhder81@gmail.com>
2021-04-03 23:17:44 +0200

    set Annotation in Model (Country & State) Closes #115

--- back-end/src/main/java/com/spring/restaurant/model/State.java
+++ back-end/src/main/java/com/spring/restaurant/model/State.java
@@ -1,4 +1,10 @@
 package com.spring.restaurant.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "state")
 public class State extends PublicData {
+    
 }
