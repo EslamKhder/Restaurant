7505e5c59bf351af954192674d1ccfa8e6d63abb
EslamKhder <eslamkhder81@gmail.com>
2021-04-03 23:26:07 +0200

    @MappedSuperclass Closes #117

--- back-end/src/main/java/com/spring/restaurant/model/Country.java
+++ back-end/src/main/java/com/spring/restaurant/model/Country.java
@@ -1,12 +1,19 @@
 package com.spring.restaurant.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "country")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Country extends PublicData{
 
     @Column(name = "code")
--- back-end/src/main/java/com/spring/restaurant/model/PublicData.java
+++ back-end/src/main/java/com/spring/restaurant/model/PublicData.java
@@ -1,7 +1,11 @@
 package com.spring.restaurant.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class PublicData extends BaseEntity {
 
-
+    @Column(name = "name")
     private String name;  // Egypt
 }
