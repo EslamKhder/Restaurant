35b5bbcea544f36a011018c338dc428a267c4ce5
EslamKhder <eslamkhder81@gmail.com>
2021-04-03 23:40:59 +0200

    Relationship between Model (Country & State) Closes #116

--- back-end/src/main/java/com/spring/restaurant/model/Country.java
+++ back-end/src/main/java/com/spring/restaurant/model/Country.java
@@ -7,15 +7,20 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Entity
 @Table(name = "country")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Country extends PublicData{
+public class Country extends PublicData {
 
     @Column(name = "code")
     private String code; // EG
+
+    @OneToMany(mappedBy = "country")
+    private Set<State> states;
 }
--- back-end/src/main/java/com/spring/restaurant/model/State.java
+++ back-end/src/main/java/com/spring/restaurant/model/State.java
@@ -1,10 +1,16 @@
 package com.spring.restaurant.model;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "state")
 public class State extends PublicData {
-    
+
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
 }
