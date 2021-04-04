e8ec9a5fc904bc66db520e890176bcb40f6c1155
EslamKhder <eslamkhder81@gmail.com>
2021-04-04 13:06:44 +0200

    get states by country Code (Controller) Closes #129

--- back-end/src/main/java/com/spring/restaurant/controller/StateController.java
+++ back-end/src/main/java/com/spring/restaurant/controller/StateController.java
@@ -3,10 +3,7 @@ package com.spring.restaurant.controller;
 import com.spring.restaurant.model.State;
 import com.spring.restaurant.service.StateService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +23,11 @@ public class StateController {
     @GetMapping("/states")
     public List<State> getStates(){
         return stateService.getAllStates();
+    }
+
+    // http://localhost:8080/api/statescode?code={value}
+    @GetMapping("/statescode")
+    public List<State> getStatesByCode(@RequestParam String code){
+        return stateService.getStatesByCountryCode(code);
     }
 }
