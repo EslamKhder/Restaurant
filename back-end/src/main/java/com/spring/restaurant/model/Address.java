ba03733bb4a84524a630a8f0f0601c89cdfb6a13
EslamKhder <eslamkhder81@gmail.com>
2021-04-26 19:48:04 +0200

    Create Model (Client - Address) Closes #142

---
+++ back-end/src/main/java/com/spring/restaurant/model/Address.java
@@ -0,0 +1,12 @@
+package com.spring.restaurant.model;
+
+public class Address {
+
+    private long id;
+
+    private String country;
+
+    private String state;
+
+    private String zipCode;
+}
---
+++ back-end/src/main/java/com/spring/restaurant/model/Client.java
@@ -0,0 +1,12 @@
+package com.spring.restaurant.model;
+
+public class Client {
+
+    private long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String phoneNumber;
+}
