6fe34b8642d0a5530b810f00c9444869730b4cd2
EslamKhder <eslamkhder81@gmail.com>
2021-04-27 23:40:30 +0200

    Create ClientRepository Closes #150

---
+++ back-end/src/main/java/com/spring/restaurant/deo/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.spring.restaurant.deo;
+
+import com.spring.restaurant.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client,Long> {
+}
