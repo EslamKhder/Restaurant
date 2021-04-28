150bd2fa7939d6dd4a11d631648d5d8d5c4147e1
EslamKhder <eslamkhder81@gmail.com>
2021-04-28 02:32:40 +0200

    Create PurchaseController Closes #153

---
+++ back-end/src/main/java/com/spring/restaurant/controller/PurchaseController.java
@@ -0,0 +1,29 @@
+package com.spring.restaurant.controller;
+
+import com.spring.restaurant.dto.PurchaseRequest;
+import com.spring.restaurant.dto.PurchaseResponse;
+import com.spring.restaurant.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+// http://localhost:8080
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/buy")
+// http://localhost:8080/api/buy
+public class PurchaseController {
+
+    private PurchaseService purchaseService;
+
+    @Autowired
+    public PurchaseController(PurchaseService purchaseService) {
+        this.purchaseService = purchaseService;
+    }
+
+
+    // http://localhost:8080/api/buy/purchase
+    @PostMapping("/purchase")
+    public PurchaseResponse addRequestOrder(@RequestBody PurchaseRequest purchaseRequest){
+        return purchaseService.addRequestOrder(purchaseRequest);
+    }
+}
