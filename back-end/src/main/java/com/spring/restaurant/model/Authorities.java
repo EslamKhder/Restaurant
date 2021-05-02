6fdedf9f187ea8c343a3716d4b9557a88569b3ec
EslamKhder <eslamkhder81@gmail.com>
2021-05-02 23:34:55 +0200

    set Annotations model (User - Authorities) Closes #165

--- back-end/src/main/java/com/spring/restaurant/model/Authorities.java
+++ back-end/src/main/java/com/spring/restaurant/model/Authorities.java
@@ -1,7 +1,23 @@
 package com.spring.restaurant.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "authorities")
 public class Authorities extends BaseEntity{
 
+    @Column(name = "role_name")
     private String roleName;
 
 }
--- back-end/src/main/java/com/spring/restaurant/model/User.java
+++ back-end/src/main/java/com/spring/restaurant/model/User.java
@@ -1,10 +1,29 @@
 package com.spring.restaurant.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
 public class User extends BaseEntity{
 
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "password")
     private String password;
 
+    @Column(name = "active")
     private int active;
+
 }
