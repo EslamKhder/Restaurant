270f5a667909bbb123726b50fc41ceb4a56dda63
EslamKhder <eslamkhder81@gmail.com>
2021-05-03 21:21:35 +0200

    SessionCreationPolicy STATELESS Closes #179

--- back-end/src/main/java/com/spring/restaurant/config/springsecurty/SpringSecurityConfig.java
+++ back-end/src/main/java/com/spring/restaurant/config/springsecurty/SpringSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -40,6 +41,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                 .antMatchers("/login").permitAll()
--- back-end/src/main/java/com/spring/restaurant/config/springsecurty/jwt/JwtAuthorizationFilter.java
+++ back-end/src/main/java/com/spring/restaurant/config/springsecurty/jwt/JwtAuthorizationFilter.java
@@ -8,7 +8,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
