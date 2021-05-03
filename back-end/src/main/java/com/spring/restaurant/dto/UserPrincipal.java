8380e2dfed19182e6765b26106945ad02c9efbf0
EslamKhder <eslamkhder81@gmail.com>
2021-05-03 04:23:10 +0200

    Complete UserPrincipal Closes #171

--- back-end/src/main/java/com/spring/restaurant/dto/UserPrincipal.java
+++ back-end/src/main/java/com/spring/restaurant/dto/UserPrincipal.java
@@ -3,9 +3,12 @@ package com.spring.restaurant.dto;
 import com.spring.restaurant.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class UserPrincipal implements UserDetails {
 
@@ -18,17 +21,22 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        user.getAuthorities().forEach(temp -> {
+            GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(temp.getRoleName());
+            authorities.add(grantedAuthority);
+        });
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getEmail();
     }
 
     @Override
@@ -48,6 +56,6 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return user.getActive() == 1;
     }
 }
